8ff9fe48b6b1c63afef753de1ffeb7fb69cca95b
hello-worldyj <sis00011086@gmail.com>
2025-12-08 20:28:17 +0700

    Update book.js

--- book.js
+++ book.js
@@ -1,10 +1,1 @@
-export default async function handler(req, res) {
-    const { title, author } = req.query;
-    const GOOGLE_KEY = process.env.GOOGLE_API_KEY;
 
-    const url = `https://www.googleapis.com/books/v1/volumes?q=intitle:${title}+inauthor:${author}&key=${GOOGLE_KEY}`;
-    const r = await fetch(url);
-    const json = await r.json();
-
-    res.status(200).json(json);
-}
